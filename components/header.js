dd049359933df0264409e54ce75011e58626bd05
Michael Mitchell <0x4d464d48@gmail.com>
2019-12-31 21:18:51 -0800

    Added Bootstrap framework

--- components/header.js
+++ components/header.js
@@ -1,7 +1,13 @@
+var bootStrapElement = React.createElement(
+  'Alert',
+  {variant: 'primary'},
+  'This is a Bootstrap Alert!'
+);
+
 var testElement = React.createElement(
   'h1',
   {},
-  'Howdly diddly ding dong doodly!'
+  'Howdly diddly ding dong doodly!', bootStrapElement
 );
 
 
